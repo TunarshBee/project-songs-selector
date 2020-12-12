6eebb1fb91e9107ecfd353b3a95b9e661ead5466
unknown <tunarshreal@gmail.com>
2020-12-12 17:20:33 -0200

    Connected the song list component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,20 +5,20 @@ const songReducer = ()=>{
         {title:"Like You", duration:'3:02'},
         {title:'Helplessly', duration:'4:13'},
         {title:'B.O.M.O', duration:'3:15'},
-        {title:'Your Ocean', duration:'3:00'},
+        {title:'Your Ocean', duration:'2:43'},
         {title:'By Your Side', duration:'4:33'},
         {title:'Made 4 2', duration:'3:45'}
     ];
 };
 
-const selectedSongReducer =(selectedSong= null, action){
+const selectedSongReducer =(selectedSong= null, action)=>{
     if (action.type === 'SONG_SELECTED') {
         return action.payload
     }
     return selectedSong
 }
 
-combineReducers({
+export default combineReducers({
     songs:songReducer,
     selectedSong: selectedSongReducer
 })
